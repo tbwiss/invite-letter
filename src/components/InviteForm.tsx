0f6a20f347042061a409da8ac19c14c9817aa6bb
Thomas Wiss <thomas.wiss@neotechnology.com>
2022-04-03 18:12:44 +0200

    grapgql test

--- src/components/InviteForm.tsx
+++ src/components/InviteForm.tsx
@@ -1,6 +1,29 @@
 import React from "react";
+import { GraphQLClient, gql } from "graphql-request";
+
+async function main() {
+  const endpoint = process.env.REACT_APP_SUPABASE_URL || "";
+
+  const graphQLClient = new GraphQLClient(endpoint, {
+    headers: {
+      authorization: `Bearer ${process.env.REACT_APP_SUPABASE_API_KEY}`,
+      apiKey: `${process.env.REACT_APP_SUPABASE_API_KEY}`,
+    },
+  });
+
+  const query = gql`
+    query {
+      name
+    }
+  `;
+
+  const data = await graphQLClient.request(query);
+  console.log(JSON.stringify(data, undefined, 2));
+}
 
 export const InviteForm = () => {
+  main().catch((error) => console.error(error));
+
   return (
     <div>
       <h4>Anmeldung</h4>
